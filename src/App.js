fab874e3185ade5692fb5b4646f7a2e10e38ac38
SHL <s2hlimi@yahoo.co.jp>
2022-09-11 20:26:11 +0900

    헤더 1,2 완성 로그인 완성 시 조건부 렌더링해야함

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Header1 from "./components/header/Header1";
+import Header2 from "./components/header/Header2";
+
 import "../src/style.css";
 import { Route, Routes } from "react-router-dom";
 import AddForm from "./pages/AddForm";
 import DetailPage from "./pages/DetailPage";
 import Login from "./pages/Login";
 
-
-
 function App() {
   return (
     <>
       <Header1 />
+      <Header2 />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/products/new" element={<AddForm />} />
