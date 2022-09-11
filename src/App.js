e6756d009944d6f8556b353e43cfe823a40ed1b3
SHL <s2hlimi@yahoo.co.jp>
2022-09-12 01:21:49 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header1 from "./components/header/Header1";
 import Header2 from "./components/header/Header2";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import AddForm from "./pages/AddForm";
+import DetailPage from "./pages/DetailPage";
 
 import "../src/style.css";
 import { Route, Routes } from "react-router-dom";
-import AddForm from "./pages/AddForm";
-import DetailPage from "./pages/DetailPage";
-import Login from "./pages/Login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header1 />
       <Header2 />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/products/new" element={<AddForm />} />
         <Route path="/products/:id" element={<DetailPage />} />
